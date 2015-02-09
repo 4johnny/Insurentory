fccd3386febec1d847b89b88aca98351694cf245
Jozef Lipovsky <j.lipovsky@me.com>
2015-02-09 12:42:53 -0800

    add core data model

---
+++ Insurentory/Insurentory.m
@@ -0,0 +1,24 @@
+//
+//  Insurentory.m
+//  Insurentory
+//
+//  Created by JoLi on 2015-02-09.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import "Insurentory.h"
+#import "Asset.h"
+
+
+@implementation Insurentory
+
+@dynamic name;
+@dynamic notes;
+@dynamic timeStamp;
+@dynamic totalValue;
+@dynamic locationLatitude;
+@dynamic locationLongitude;
+@dynamic locationDescription;
+@dynamic assets;
+
+@end
